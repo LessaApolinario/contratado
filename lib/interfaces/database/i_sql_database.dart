5e44010c146a13a3a975948a2f989f890accccd7
Lessa Apolinario <lessaapolinario@gmail.com>
2022-11-06 13:59:03 -0300

    Using toJSON on user register methods

--- lib/interfaces/database/i_sql_database.dart
+++ lib/interfaces/database/i_sql_database.dart
@@ -3,9 +3,9 @@ import 'package:contratado/interfaces/database/mixins/i_register_database.dart';
 import 'package:contratado/interfaces/database/mixins/i_session_database.dart';
 import 'package:contratado/interfaces/database/mixins/i_searcher_database.dart';
 
-abstract class ISQLDatabase<Entity>
+abstract class ISQLDatabase
     with
         IAccessDatabase,
-        IRegisterDatabase<Entity>,
+        IRegisterDatabase,
         ISessionDatabase,
         ISearcherDatabase {}
